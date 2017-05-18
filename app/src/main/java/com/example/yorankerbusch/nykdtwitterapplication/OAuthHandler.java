50751dd30ca22e37419b69c913ca3e39af4bb4f0
HanieDo <Ngocnhi31101998@gmail.com>
2017-05-18 16:53:50 +0200

    Subclasses for OAuth

---
+++ app/src/main/java/com/example/yorankerbusch/nykdtwitterapplication/OAuthHandler.java
@@ -0,0 +1,16 @@
+package com.example.yorankerbusch.nykdtwitterapplication;
+
+import com.github.scribejava.core.model.OAuth1AccessToken;
+import com.github.scribejava.core.model.OAuth1RequestToken;
+
+/**
+ * Created by LENOVO on 5/18/2017.
+ */
+
+public class OAuthHandler {
+    private OAuth1AccessToken auth1AccessToken;
+    private OAuth1RequestToken auth1RequestToken;
+
+    public OAuthHandler(){
+    }
+}
